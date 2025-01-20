3e5680612f0b5e310ea7ba4087f1f87e0ecd41ad
any-git <bosuutap@alwaysdata.net>
2025-01-20 12:05:58 +0000

    ...

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.core.os_manager import ChromeType
 
 
-@st.cache
+@st.cache_resource
 def get_driver():
     options = Options()
     options.add_argument("--disable-gpu")
