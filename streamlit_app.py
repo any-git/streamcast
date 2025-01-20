d8377bb5f4dc31daefac9de643093bea7dfcdcb8
any-git <bosuutap@alwaysdata.net>
2025-01-20 12:13:56 +0000

    ...

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,11 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.core.os_manager import ChromeType
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+
+
+capabilities = DesiredCapabilities.CHROME
+capabilities["goog:loggingPrefs"] = {"performance": "ALL"}
 
 
 @st.cache_resource
@@ -14,6 +19,7 @@ def get_driver():
     return webdriver.Chrome(
         service=Service(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()),
         options=options,
+        desired_capabilities=capabilities,
     )
 
 
@@ -23,4 +29,16 @@ if url:
     driver = get_driver()
     driver.get(url)
     screenshot = driver.get_screenshot_as_png()
+    logs = driver.get_log("performance")
     st.image(screenshot)
+    st.write("Logs:")
+    for entry in logs:
+        message = entry["message"]
+        try:
+            # Parse JSON để lấy URL
+            url = eval(message)["message"]["params"]["request"]["url"]
+            st.code(url)
+        except:
+            continue
+
+    driver.quit()
