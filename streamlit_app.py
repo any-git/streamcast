f13e9f6eaa56402a520911afdaff47871fc6e76c
any-git <bosuutap@alwaysdata.net>
2025-01-20 12:05:35 +0000

    ...

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,27 +1,24 @@
 import streamlit as st
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.core.os_manager import ChromeType
 
 
-with st.echo():
-    from selenium import webdriver
-    from selenium.webdriver.chrome.options import Options
-    from selenium.webdriver.chrome.service import Service
-    from webdriver_manager.chrome import ChromeDriverManager
-    from webdriver_manager.core.os_manager import ChromeType
-
-    @st.cache_resource
-    def get_driver():
-        return webdriver.Chrome(
-            service=Service(
-                ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()
-            ),
-            options=options,
-        )
-
+@st.cache
+def get_driver():
     options = Options()
     options.add_argument("--disable-gpu")
     options.add_argument("--headless")
+    return webdriver.Chrome(
+        service=Service(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()),
+        options=options,
+    )
 
+
+with st.echo():
     driver = get_driver()
     driver.get("http://example.com")
-
-    st.code(driver.page_source)
+    screenshot = driver.get_screenshot_as_png()
+    st.image(screenshot)
